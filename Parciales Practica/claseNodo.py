1e6373053ade0548241e7cc77cd3fb59bb351668
Tams <tamarabrizuela882@gmail.com>
2025-09-29 11:37:14 -0300

    Create claseNodo.py

---
+++ Parciales Practica/claseNodo.py
@@ -0,0 +1,16 @@
+class Nodo:
+    __elemento: None
+    __sig: None
+    
+    def __init__ (self, e):
+        self.__elemento = e
+        self.__sig = None
+
+    def obtener_elemento(self):
+        return self.__elemento
+    def obtener_sig(self):
+        return self.__sig
+    def set_sig(self, valor):
+        self.__sig = valor
+    def set_elemento (self, valor):
+        self.__elemento = valor
